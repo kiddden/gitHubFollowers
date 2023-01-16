440535bc9a74973af79ea537f6acaced163484cd
kiddden <kazzer@knu.ua>
2023-01-16 20:02:15 +0200

    designed searchVC

--- GitHub Followers/GFTextField.swift
+++ GitHub Followers/GFTextField.swift
@@ -38,7 +38,7 @@ class GFTextField: UITextField {
         
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
-        
+        returnKeyType = .go
     }
 
 }
--- GitHub Followers/SearchVC.swift
+++ GitHub Followers/SearchVC.swift
@@ -8,10 +8,70 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField(placeholder: "Enter a username", borderColor: UIColor.systemGray4.cgColor)
+    let actionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureImageLogoView()
+        configureTextField()
+        congifureButton()
+        createKeyboardDismissGesture()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func createKeyboardDismissGesture() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func configureImageLogoView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        usernameTextField.delegate = self
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+    
+    func congifureButton() {
+        view.addSubview(actionButton)
+        
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -48),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            actionButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+}
 
-        view.backgroundColor = .systemBlue
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Did tap return")
+        return true
     }
 }
